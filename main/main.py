5c8c7891ed72e61077bfafd377f2a6a096fc7f22
Владислав Клюев <klyuev.vladislav01@mail.ru>
2024-10-24 14:58:52 +0300

    Поправлены пути pathlib PurePath

--- main/main.py
+++ main/main.py
@@ -1,14 +1,14 @@
 from PyQt5.QtWidgets import QApplication
 import sys
-from pathlib import Path
+from pathlib import Path, PurePath
 
-sys.path.append(Path.cwd().parent)
+sys.path.append(str(PurePath(Path.cwd().parent)))
 from view import CameraApp2
 from controller import MainController
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    app.setStyleSheet(Path(Path.cwd().parent / 'view' / 'styleSheet.qss').read_text())
+    app.setStyleSheet(Path(PurePath(Path.cwd().parent, 'view', 'styleSheet.qss')).read_text())
     ex = CameraApp2()
     main_controller = MainController(ex)
     ex.show()
